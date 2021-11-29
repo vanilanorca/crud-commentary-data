4471bbca565bf9edecfcf6518042e93882d67070
vanilanorca <vilopesalves@hotmail.com>
2021-11-29 17:44:35 -0300

    a

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
 
-server.use(middlewares)
-server.use(router)
-server.listen(process.env.PORT || 8080, () => {
-  console.log('JSON Server is running')
-})
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 8080;
+
+server.use(middlewares);
+//server.use('/api/v1', middlewares)
+server.use(jsonServer.bodyParser); //coloquei parar ver se mantém os dados
+//server.use('/api/v1', router)
+server.use(router);
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`JSON Server is running in ${port}`);
+});
